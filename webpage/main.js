fbc336b431ab96bce7c0d0d599477ded5cd32ceb
IvetaBru <ivetamarkeviciute@gmail.com>
2025-02-14 08:17:58 +0200

    kortelių atvaizdavimas

---
+++ webpage/main.js
@@ -0,0 +1,15 @@
+import Card from "./modules/Card.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.querySelector('#container');
+
+        data.motociklai.forEach(moto => {
+            const motoCard = new Card(moto);
+            container.appendChild(motoCard.render());
+        });
+    })
+    .catch(error => console.error('Klaida', error));
+});
