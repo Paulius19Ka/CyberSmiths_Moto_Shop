46a0686d47b65b965674e27db6305531b5ee1a59
Paulius19Ka <kallabow2@gmail.com>
2025-02-17 09:58:10 +0200

    created a modal for terms

--- webpage/main.js
+++ webpage/main.js
@@ -1,12 +1,19 @@
 import Card from "./modules/Card.js";
 import HeroModal from "./modules/HeroModal.js";
+import TosModal from "./modules/TosModal.js";
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
+    if(window.localStorage.getItem('Terms') !== 'agreed'){
+        new TosModal();
+    }
+
     fetch('data.json')
     .then(response => response.json())
     .then(data => {
         const container = document.querySelector('#container');
-
+        
         data.motociklai.forEach(moto => {
             const motoCard = new Card(moto);
             container.appendChild(motoCard.render());
